8368b8290d8d3e1a5d2e2cc1b2bf5cbfb7a9fcb4
Sajtospoga01 <abodrogai@gmail.com>
2024-08-04 11:39:05 +0100

    chore: Update backup script to include check for miniconda3 in PATH

--- backup.ps1
+++ backup.ps1
@@ -14,8 +14,8 @@ if (!$env:POSH_THEMES_PATH) {
     winget install OhMyPosh.OhMyPosh
 }
 
-if (!$env:Path.Contains("anaconda3")){
-    Write-Output  
+if (!$env:Path.Contains("anaconda3") -or !$env:Path.Contains("miniconda3")){
+    Write-Output "  - Anaconda 3" 
 }else {
     Write-Output "  - anaconda already installed"
 }
